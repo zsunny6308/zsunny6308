88779b4e96261d0871c1b11a543936ca51edc7bd
zsunny6308 <113335935+zsunny6308@users.noreply.github.com>
2022-09-16 16:05:17 +0530

    Add files via upload

---
+++ array operations.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Function to implement search operation
+int findElement(int arr[], int n, int key)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        if (arr[i] == key)
+            return i;
+     
+      
+    return -1;
+}
+ 
+// Driver's Code
+int main()
+{
+    int arr[] = { 12, 34, 10, 6, 40 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    
+    int key = 40;
+   
+      
+    int position = findElement(arr, n, key);
+ 
+    if (position == -1)
+        cout << "Element not found";
+    else
+        cout << "Element Found at Position: "
+             << position + 1;
+ 
+    return 0;
+}
